93fd00e2d8cdbfa4f815347451102e8927d4e022
sergeyzlenskiy <50332094+sergeyzlenskiy@users.noreply.github.com>
2021-12-09 20:00:24 +0300

    Update validator.js

--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,6 @@
 export default class Validator {
   static validateUsername(expression) {
-    const regex = new RegExp('^[a-zA-Z]+[0-9]{0,3}_{0,2}\\-{0,2}[a-zA-Z]+$');
+    const regex = new RegExp('^[a-zA-Z]+([a-zA-Z]+|([0-9]{0,3})|(-*\\_*))*[a-zA-Z]+$');
     return regex.test(expression);
   }
 }
